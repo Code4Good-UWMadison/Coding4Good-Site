99d06c29559d6f5a142b6804bc6ddf1cddf9bf36
Yuanchen Li <yli@y-l.me>
2017-11-30 19:00:32 -0600

    Absolute Ref

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/api', api);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/proposal', function(req, res, next) {
+  res.json({});
+});
+
+module.exports = router;
