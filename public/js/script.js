f240624daf4195521e1d8a506bdbe42f6c76b4a8
Jialuo Gao <jialuogao980513@gmail.com>
2018-01-27 20:46:09 -0600

    minor changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-  $("a").click(function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+// $(document).ready(function(){
+//   $("a").click(function(event) {
+//
+//     // Make sure this.hash has a value before overriding default behavior
+//     if (this.hash !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+//
+//       // Store hash
+//       var hash = this.hash;
+//
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
+//         // Add hash (#) to URL when done scrolling (default click behavior)
+//         window.location.hash = hash;
+//       });
+//     } // End if
+//   });
 
   $("#proposal").click(function(event){
     var wind = window.open("/proposal", "popupWindow", "width=900,height=580,scrollbars=yes");
