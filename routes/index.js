e4b3412823dd590cd4f26418cbfec3a205816e5b
Yuanchen Li <yli@y-l.me>
2018-02-08 01:25:39 -0600

    Peripheral Pages

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', {});
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', {});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {});
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {});
+});
+
 router.get('/proposal', function(req, res){
   res.render('proposal');
 });
