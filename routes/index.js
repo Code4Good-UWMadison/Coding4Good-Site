671b676cfb00f2cdfc4d9ee708d5762e3bd41dc2
Shawn Zhong <public@shawnzhong.com>
2018-03-03 20:47:09 -0600

    Minor

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require("./server/db");
+var db = require("../server/db");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
