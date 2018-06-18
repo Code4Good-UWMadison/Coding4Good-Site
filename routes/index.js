4289868055a03039d24f8bce5643b82262800013
Chentao Wang <cwang556@wisc.edu>
2018-06-18 02:13:04 -0500

    Add event create/edit/remove. Unfinished & untested. Need help QAQ

--- routes/index.js
+++ routes/index.js
@@ -172,4 +172,37 @@ router.get('/events', function (req, res, next) {
     });
 });
 
+router.get('/events/new', function (req, res, next) {
+    if (req.session.uid == null) {
+        res.redirect('/login');
+        return;
+    }
+    if (req.session.uid != 1) {
+        res.redirect('/events');
+        return;
+    }
+        //Not quite sure what is the part inside render
+        res.render('eventsCreate', {});
+});
+
+router.get('/events/edit', function (req, res, next) {
+    if (req.session.uid == null) {
+        res.redirect('/login');
+        return;
+    }
+    if (req.session.uid != 1) {
+        res.redirect('/events');
+        return;
+    }
+    db.getEventById(req.query.id, function (err, event) {
+        if (err) {
+            console.log(err);
+            res.status(400).json({msg: 'Database Error'});
+            return;
+        }
+        //Not quite sure what is the part inside render
+        res.render('eventEdit', {});
+    });
+});
+
 module.exports = router;
