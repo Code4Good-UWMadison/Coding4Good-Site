0e0556917ac19408f1f354b6b42e9bba4dbcddab
jialuogao <jialuogao980513@gmail.com>
2019-12-06 15:37:33 -0600

    Fall 2019 web dev (#17)
    * Add sponsor page (#9)
    
    * Disabled signup and fix time to CST and added create date for signup
    
    * hot fix for minor bug
    
    * more hotfix
    
    * minor
    
    * Fixed popup
    
    * disabled broken contact
    
    * remove sponsor-container class
    
    * add a new sponsor page
    
    * add sponsor icons on the main page (#10)
    
    * add sponsor icons on the main page
    
    * change ms-icon to the one without text
    
    * BG color style fix
    
    * Finished password hashing and re-enabled sign up (#12)
    
    * finished password hashing and re-enabled password
    
    * fix contact and about link in proposal page (#14)
    
    * About Page Update - New pull request (#15)
    
    * TeamMember div update from Haoming
    
    * Add sponsor page (#9)
    
    * Disabled signup and fix time to CST and added create date for signup
    
    * hot fix for minor bug
    
    * more hotfix
    
    * minor
    
    * Fixed popup
    
    * disabled broken contact
    
    * remove sponsor-container class
    
    * add a new sponsor page
    
    * About page updated
    
    * Minor fix
    
    * add sponsor icons on the main page (#10)
    
    * add sponsor icons on the main page
    
    * change ms-icon to the one without text
    
    * Finished password hashing and re-enabled sign up (#12)
    
    * finished password hashing and re-enabled password
    
    * fix contact and about link in proposal page (#14)
    
    * Update with minor change
    
    * Fix minor issues
    
    * Delete unnecessary package
    
    * Minor updates
    
    * Email verification finished  (#16)
    
    * finished password hashing and re-enabled password
    
    * Minor typo fix
    
    * remove stuff
    
    * Half way done, half to go
    
    * fixed bug
    
    * Confirmed page not working
    
    redirect after signup
    
    * email verification done, without setup
    
    * Setup update
    
    * Minor
    
    * Minor changes
    
    * finished
    
    * Add remove user and minor adjusts
    
    * Delete buggy code
    
    * Account check message and minor updates
    
    * remove unnecessary prints
    
    * Minor footer fix
    
    * Remove icon bg (#18)
    
    * add an icon that does not have background color
    
    * display icon without bg in email confirmation page
    
    * typo
    
    * Icon in email

--- server/index_db.js
+++ server/index_db.js
@@ -2,6 +2,7 @@ var self = this;
 var pg = require('pg');
 var config = require('./dbconfig.js');
 var index_db = new pg.Pool(config.db);
+var bcrypt = require('bcrypt');
 
 // exports.reset = function (callback) {
 //   var query = `drop table if exists user_profile;
@@ -40,38 +41,74 @@ var index_db = new pg.Pool(config.db);
 // };
 
 exports.createUser = function (user, callback) {
+  // the higher the saltRound the longer to hash the password
+  var saltRounds = 10;
+  bcrypt.hash(user.password, saltRounds, function(err, hash){
+    if(err){
+      console.log(err);
+      callback(err);
+    }
+    var query = `INSERT INTO users (email, name, password, create_date, email_verified) VALUES ($1,$2,$3,now() at time zone 'America/Chicago', false);`;
+    index_db.query(query, [user.email, user.fullname, hash], function (err, result) {
+      if (err) {
+        if (err.code != 23505){
+          console.log(err);
+        }
+        callback(err);
+      }
+      else{
+        exports.verifyUser(user, callback);
+      }
+    });
+  });
+};
 
-  var query = `insert into users (email, name, password, create_date) values($1,$2,$3,now() at time zone 'America/Chicago');`;
-  index_db.query(query, [user.email, user.fullname, user.password], function (err, result) {
-    if (err) {
+exports.removeUser = function (uid, callback){
+  var query = `DELETE FROM users WHERE id = $1;`;
+  index_db.query(query, [uid], function (err) {
+    if(err) {
       console.log(err);
       callback(err);
     }
     else {
-      exports.verifyUser(user, callback);
+      callback(null);
     }
   });
-};
+}
 
 exports.verifyUser = function (user, callback) {
-    var query = `select id from users where email=$1 and password=$2;`;
-    index_db.query(query, [user.email, user.password], function (err, result) {
-        if (err) {
+  var query = `SELECT id, password FROM users WHERE email = $1;`;
+  index_db.query(query, [user.email], function(err, result) {
+    if (err) {
+      console.log(err);
+      callback(err);
+    }
+    else{
+      // if user is not found
+      if (result.rows.length == 0) {
+        callback(null, null);
+      }
+      else {
+        bcrypt.compare(user.password, result.rows[0].password, function(err, res) {
+          if(err){
+            console.log(err);
             callback(err);
-        }
-        else {
-            if (result.rows.length == 0) {
-                callback(null, null);
-            }
-            else {
-                callback(null, result.rows[0].id);
-            }
-        }
-    });
-};
+          }
+          // if wrong password
+          else if (!res) {
+            callback(null, null);
+          }
+          else{
+            callback(null, result.rows[0].id);
+          }
+        });
+      }
+    }
+  });
+}
 
 exports.getUserInfo = function (uid, callback) {
-  var query = `select email,name as fullname from users where id=$1;`;
+  var query = `SELECT email, name AS fullname FROM users WHERE id=$1;`;
   index_db.query(query, [uid], function (err, result) {
     if (err) {
       callback(err);
@@ -88,11 +125,10 @@ exports.getUserInfo = function (uid, callback) {
 };
 
 exports.updateProfile = function (uid, profile, callback) {
-  var query = `insert into user_profile (uid, nickname, year, intended_teamleader, pl, dev, resume, submission_time) values($1,$2,$3,$4,$5,$6,$7, now() at time zone 'America/Chicago');`;
+  var query = `INSERT INTO user_profile (uid, nickname, year, intended_teamleader, pl, dev, resume, submission_time) VALUES ($1,$2,$3,$4,$5,$6,$7, now() at time zone 'America/Chicago');`;
   index_db.query(query, [uid, profile.nickname, profile.year, profile.intended_teamleader, profile.pl, profile.dev, (profile.resume) ? profile.resume : ''], function (err, result) {
     if (err) {
       callback(err);
-
     }
     else {
       callback(null);
@@ -101,19 +137,18 @@ exports.updateProfile = function (uid, profile, callback) {
 };
 
 exports.getProfile = function (pid, callback) {
-  var query = `select
-      u.name as name,
-      n.nickname as nickname,
-      n.year as year,
-      n.intended_teamleader as intended_teamleader,
-      n.pl as pl,
-      n.dev as dev,
-      n.resume as resume
-    from user_profile as n, users as u where n.id=$1 and n.uid=u.id;`;
+  var query = `SELECT
+      u.name AS name,
+      n.nickname AS nickname,
+      n.year AS year,
+      n.intended_teamleader AS intended_teamleader,
+      n.pl AS pl,
+      n.dev AS dev,
+      n.resume AS resume
+      FROM user_profile AS n, users AS u WHERE n.id=$1 AND n.uid=u.id;`;
   index_db.query(query, [pid], function (err, result) {
     if (err) {
       callback(err);
-
     }
     else {
       if (result.rows.length > 0) {
@@ -125,4 +160,33 @@ exports.getProfile = function (pid, callback) {
     }
   });
 };
+
+exports.verifyEmailByUserId = function (uid, callback){
+  var query = `UPDATE users SET email_verified = true WHERE id = $1;`;
+  index_db.query(query, [uid], function (err) {
+    if (err) {
+      callback(err);
+    }else{
+      callback(null);
+    }
+  });
+};
+
+exports.checkEmailVerificationByUid = function (uid, callback){
+  var query = `SELECT email_verified FROM users WHERE id = $1;`;
+  index_db.query(query, [uid], function(err, result){
+    if (err){
+      callback(err);
+    }
+    else{
+      if (result.rows.length > 0){
+        callback(null, result.rows[0].email_verified);
+      }
+      else{
+        callback(null);
+      }
+    }
+  });
+};
+
 
