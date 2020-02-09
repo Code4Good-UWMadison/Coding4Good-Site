950713f197489cbc7ea2d97e270f6517ba61b1db
Harry Liu <harry881218@gmail.com>
2020-02-08 18:12:56 -0600

    applyProject v1

--- routes/project/project_api.js
+++ routes/project/project_api.js
@@ -3,6 +3,7 @@ var db = require('../../server/project_db');
 var router = express.Router();
 
 const authService = require('../services/authorization_service');
+const msg = $("#error-msg");
 
 router.post('/createProject', function (req, res, next) {
     let roles = [authService.UserRole.Root, 
@@ -77,4 +78,36 @@ router.post('/removeProject', function(req, res, next){
     });
 });
 
+router.post('/applyProject', function(req, res, next){
+    let roles = null;
+    authService.authorizationCheck(roles, req.session.uid, function(err, authorized){
+        if (err) {
+            res.status(400).json({msg: 'Database Error'});
+            return;
+        }
+        else if(!authorized){
+            res.status(400).json({msg: 'Not Authorized'});
+            return;
+        }
+        db.getProfileByUserId(req.session.uid, function(err, hasProfile){
+            if (err){
+                res.status(400).json({msg: "Database Error"});
+            }else{
+                if (hasProfile){
+                    db.applyProject(req.body.project_id, req.session.uid, function(err){
+                        if(err){
+                            console.log(err);
+                            res.status(400).json({msg: 'Failed to apply'});
+                            return;
+                        }
+                        res.json({});
+                    });
+                }
+                else{
+                    res.json({msg: "Please finish your Profile before applying for a project."});
+                }
+            }
+        });
+    });
+});
 module.exports = router;
