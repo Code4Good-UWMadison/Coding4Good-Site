7d8038e731ca4c06d35e759b8ccd3383bab59d87
Jialuo Gao <jialuogao980513@gmail.com>
2020-02-10 16:09:04 -0600

    update to userrole

--- public/js/enum.js
+++ public/js/enum.js
@@ -1,7 +1,3 @@
 var ProjectStatus = {
     Starting:"Starting", InProgress:"In Progress", OnHold:"On Hold", Succeed:"Succeed", Failed:"Failed", Maintaining:"Maintaining"
 };
-
-var UserRole = {
-    Root:"Root", Admin:"Admin", EventExecutive:"Event Executive", ProjectManager:"Project Manager", ProjectLeader:"Project Leader", ProjectMember:"Project Member", Finance:"Finance", Outreach:"Outreach"
-};
