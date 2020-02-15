ab31dfaaa649164b34e51b26840e4ab1820b3a0b
Jialuo Gao <jialuogao980513@gmail.com>
2020-02-15 14:48:40 -0600

    bug fix, added see team lead

--- routes/project/project_api.js
+++ routes/project/project_api.js
@@ -184,24 +184,25 @@ router.post('/rejectApplicant', function(req, res, next){
                             res.status(404).json({msg: "Not found"});
                         }
                         else{
-                            const url = `https://${baseUrl}/project/detail?id=${project_id}`;
-                            const emailDetail = {
-                                to: req.body.user.email,
-                                subject: "Project application result from Coding4Good",
-                                html: `Thank you for your interest in project ${project.title}.&nbsp;<br>
-                                        Unfortunately, the team ${project.title} will not move on with your application.&nbsp;<br>
-                                        Please wait for responses from other teams.&nbsp;<br>
-                                        Also, please follow the link to contact your Project Leader for any questions!&nbsp;<br>
-                                        <a href='${url}'>${url}</a>`
-                            };
-                            emailService.sendEmail(emailDetail, function(err){
-                                if(err){
-                                    console.log(err);
-                                    res.status(400).json({msg: 'Failed to send email'});
-                                }else{
-                                    res.json({});
-                                }
-                            });
+                            // const url = `https://${baseUrl}/project/detail?id=${project_id}`;
+                            // const emailDetail = {
+                            //     to: req.body.user.email,
+                            //     subject: "Project application result from Coding4Good",
+                            //     html: `Thank you for your interest in project ${project.title}.&nbsp;<br>
+                            //             Unfortunately, the team ${project.title} will not move on with your application.&nbsp;<br>
+                            //             Please wait for responses from other teams.&nbsp;<br>
+                            //             Also, please follow the link to contact your Project Leader for any questions!&nbsp;<br>
+                            //             <a href='${url}'>${url}</a>`
+                            // };
+                            // emailService.sendEmail(emailDetail, function(err){
+                            //     if(err){
+                            //         console.log(err);
+                            //         res.status(400).json({msg: 'Failed to send email'});
+                            //     }else{
+                            //         res.json({});
+                            //     }
+                            // });
+                            res.json({});
                         }
                     });
                 }
