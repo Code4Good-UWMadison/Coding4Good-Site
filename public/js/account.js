9ad882b97493e9d0718273152f0ea2ed8fbf49fc
Yi Chen <ychen878@wisc.edu>
2020-03-18 19:46:55 -0500

    shorten the username if the name is very long

--- public/js/account.js
+++ public/js/account.js
@@ -6,8 +6,33 @@ $(function () {
     data: {},
     success: function (data) {
       if (data.id != -1) {
+        var displaynName = data.fullname;
+        let maxLength = 14;
+        // if the name is very long, change the way of displaying the image
+        if (displaynName.length > maxLength) {
+          var res = displaynName.split(" ");
+          displaynName = "";
+          var currentLength = 0;
+          if (res[0].length < 14) {
+            res.forEach(function (sub) {
+              currentLength += sub.length;
+              if (currentLength > maxLength - 3) {
+                displaynName += " ";
+                displaynName += sub.charAt(0);
+                displaynName += ".";
+                return;
+              }
+              displaynName += sub;
+              displaynName += " ";
+            });
+          } else {
+            displaynName = data.fullname.slice(0, 12);
+            displaynName += ".";
+          }
+        }
+        console.log(displaynName);
         $(".navbar-right").html(`<ul class="nav navbar-nav">
-          <li><a>Welcome! ` + data.fullname + `</a></li>
+          <li><a>Welcome! ` + displaynName + `</a></li>
           <li><a>|</a></li>
           <li><a href="/profile">Profile</a></li>
           <li><a>|</a></li>
