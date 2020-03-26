57d8b684970e965cdf15d355afce78f41cec30d3
Yi Chen <ychen878@wisc.edu>
2020-03-26 15:40:26 -0500

    minor

--- public/js/account.js
+++ public/js/account.js
@@ -6,32 +6,32 @@ $(function () {
     data: {},
     success: function (data) {
       if (data.id != -1) {
-        var displayingName = data.fullname;
+        var display_name = data.fullname;
         let maxLength = 14;
-        // if the name is very long, change the way of displaying the image
-        if (displayingName.length > maxLength) {
-          var res = displayingName.split(" ");
-          displayingName = "";
+        // if the name is very long, change the way of displaying the name
+        if (display_name.length > maxLength) {
+          var res = display_name.split(" ");
+          display_name = "";
           var currentLength = 0;
           if (res[0].length < 14) {
             res.forEach(function (sub) {
               currentLength += sub.length;
               if (currentLength > maxLength - 3) {
-                displayingName += " ";
-                displayingName += sub.charAt(0);
-                displayingName += ".";
+                display_name += " ";
+                display_name += sub.charAt(0);
+                display_name += ".";
                 return;
               }
-              displayingName += sub;
-              displayingName += " ";
+              display_name += sub;
+              display_name += " ";
             });
           } else {
-            displayingName = data.fullname.slice(0, 12);
-            displayingName += ".";
+            display_name = data.fullname.slice(0, 12);
+            display_name += ".";
           }
         }
         $(".navbar-right").html(`<ul class="nav navbar-nav">
-          <li><a>Welcome! ` + displayingName + `</a></li>
+          <li><a>Welcome! ` + display_name + `</a></li>
           <li><a>|</a></li>
           <li><a href="/profile">Profile</a></li>
           <li><a>|</a></li>
