7779c21cdb4a4ede91982d3bb535245c66649a7b
Reid Chen <mrr1vfe@gmail.com>
2020-10-16 17:37:34 -0500

    Update enum.js
    add status flag, EventStatus

--- public/js/enum.js
+++ public/js/enum.js
@@ -1,3 +1,14 @@
 var ProjectStatus = {
     Starting:"Starting", InProgress:"In Progress", OnHold:"On Hold", Succeed:"Succeed", Failed:"Failed", Maintaining:"Maintaining"
 };
+
+/*
+| status code |  meaning  |
+|-------------|-----------|
+|      0      |   active  |
+|      1      | cancelled |
+ */
+const EventStatus = {
+    Active: 0,
+    Cancelled: 1
+}
