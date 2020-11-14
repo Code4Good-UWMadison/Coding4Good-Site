839ce87b0fd7d90dad7c9da0228564dca65fe596
Carissa Woolman <carissaawoolman@gmail.com>
2020-11-13 18:28:47 -0600

    Finished transactions pt.3

--- routes/proposal/proposal_api.js
+++ routes/proposal/proposal_api.js
@@ -4,37 +4,38 @@ const emailService = require('../services/email_service');
 var router = express.Router();
 
 router.post('/createProposal', function (req, res, next) {
-    let proposal = req.body;
-    db.createProposal(proposal).then(() => {
-        console.log("44444444444444444444123");
-        let emailDetail = {
-            subject: 'Proposal from the website',
-            html: '<h4>Org Info: <br> <hr>'
-            + 'Org Name: ' + proposal.org_name + '<br>'
-            + 'Organization: ' + proposal.org_type + '<br>'
-            + 'Email: ' + proposal.org_detail + '<br>'
-            + 'Proposer Info: <br> <hr>'
-            + 'Phone: ' + proposal.name + '<br>'
-            + 'Date: ' + proposal.position + '<br>'
-            + 'UserId: ' + proposal.email + '<br>'
-            + 'Other Contact: ' + proposal.contact + '<br>'
-            + 'Proposal Info: <br> <hr>' 
-            + 'Proposal title: ' + proposal.title + '<br>' 
-            + 'proposal description: <p>' + proposal.description + '</p></h4>',
-        };
-        emailService.sendEmail(emailDetail, function(err) {
-            if (err) {
-                console.log(err);
-            }
-            else{
-                res.json({});
-            }
-        });
-    }).catch(err => {
-        console.log("444444444444444444444321");
-        console.log(err);
-        res.status(400).json({msg: 'Failed to create'});
-        return;
+    var proposal = req.body;
+    db.createProposal(proposal, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(400).json({msg: 'Failed to create'});
+            return;
+        }
+        else {
+            var emailDetail = {
+                subject: 'Proposal from the website',
+                html: '<h4>Org Info: <br> <hr>'
+                + 'Org Name: ' + proposal.org_name + '<br>'
+                + 'Organization: ' + proposal.org_type + '<br>'
+                + 'Email: ' + proposal.org_detail + '<br>'
+                + 'Proposer Info: <br> <hr>'
+                + 'Phone: ' + proposal.name + '<br>'
+                + 'Date: ' + proposal.position + '<br>'
+                + 'UserId: ' + proposal.email + '<br>'
+                + 'Other Contact: ' + proposal.contact + '<br>'
+                + 'Proposal Info: <br> <hr>' 
+                + 'Proposal title: ' + proposal.title + '<br>' 
+                + 'proposal description: <p>' + proposal.description + '</p></h4>',
+            };
+            emailService.sendEmail(emailDetail, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+                else{
+                    res.json({});
+                }
+            });
+        }
     });
 });
 
--- server/proposal_db.js
+++ server/proposal_db.js
@@ -1,52 +1,15 @@
 var self = this;
-const pg = require('pg');
-const config = require('./dbconfig.js');
-const db = new pg.Pool(config.db);
+var pg = require('pg');
+var config = require('./dbconfig.js');
+var db = new pg.Pool(config.db);
 
-exports.createProposal = async (proposal) => {
-    // db.query(query, [proposal.org_name, proposal.org_type, proposal.org_detail, proposal.name, proposal.position, proposal.email, proposal.contact, proposal.title, proposal.description], function (err) {
-      //     if (err) {
-        //         console.log(err);
-        //         callback(err);
-        //     }
-        //     callback(null);
-        //});
-        // (async () => {
-          // note: we don't try/catch this because if connecting throws an exception
-          // we don't need to dispose of the client (it will be undefined)
-          //(err, client, release) => {
-            //     console.log("hihihhihiihihihihi");
-            //     if (err) {
-              //       return console.error('Error acquiring client', err.stack)
-              //     }
-              //     client.query('SELECT NOW()', (err, result) => {
-                //         console.log("454454445454545445454545454");
-                //         release()
-                //       if (err) {
-                  //         return console.error('Error executing query', err.stack)
-                  //       }
-                  //       console.log(result.rows)
-                  //     })
-                  //   })
-    console.log("444444444444444444444");
-    const client = await db.connect();
-    try {
-      console.log("444444444444444444443");
-      await client.query('BEGIN');
-      var query = `insert into proposal (org_name, org_type, org_detail, name, position, email, contact, title, description, creation_time) values($1,$2,$3,$4,$5,$6,$7,$8,$9,now() at time zone 'America/Chicago')`;
-      await client.query(query, [proposal.org_name, proposal.org_type, proposal.org_detail, proposal.name, proposal.position, proposal.email, proposal.contact, proposal.title, proposal.description]);
-      await client.query('COMMIT');
-
-    } catch (err) {
-      console.log("444444444444444444442");
-      await client.query('ROLLBACK');
-      console.log(err);
-      throw err;
-      //callback(err);
-      
-    } finally {
-      console.log("444444444444444444441");
-      client.release();
-      //callback(null);
-    }
+exports.createProposal = function (proposal, callback) {
+    var query = `insert into proposal (org_name, org_type, org_detail, name, position, email, contact, title, description, creation_time) values($1,$2,$3,$4,$5,$6,$7,$8,$9,now() at time zone 'America/Chicago')`;
+    db.query(query, [proposal.org_name, proposal.org_type, proposal.org_detail, proposal.name, proposal.position, proposal.email, proposal.contact, proposal.title, proposal.description], function (err) {
+        if (err) {
+            console.log(err);
+            callback(err);
+        }
+        callback(null);
+    });
 };
