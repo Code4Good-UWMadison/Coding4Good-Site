60473829cd21e2415c82786cd87ebcbd3d34c030
carissawoolman <carissaawoolman@gmail.com>
2020-11-25 22:33:05 -0600

    Nodejs transactions (#37)
    * proposal_db transactions
    
    
    update
    
    
    removed file
    
    * ApproveApplicant finished
    
    * setUserRoleByUid not finished
    
    * Finished creating project update
    
    * update project api
    
    * Minor
    
    * Finished transactions
    
    * Finished transactions pt.2
    
    * Finished transactions pt.3
    
    * minor auth update
    
    * Comments on PR fixed
    
    Co-authored-by: Jialuo Gao <jialuogao980513@gmail.com>

--- server/project_db.js
+++ server/project_db.js
@@ -3,13 +3,14 @@ var pg = require("pg");
 var config = require("./dbconfig.js");
 var db = new pg.Pool(config.db);
 
-exports.editProject = function (project, callback) {
+exports.editProject = async (project) => {
     const query = `update project set title=$2, description=$3, contact=$4, org_name=$5, status=$6, project_link=$7, video_link=$8, applyable=$9 where project.id = $1;`;
     const oldLink = `DELETE FROM user_role WHERE user_role.associated_project_id = $1`;
     const newLink = `INSERT INTO user_role(associated_project_id, user_id, user_role) values($1,$2,$3);`;
-    db.query(
-        query,
-        [
+    const client = await db.connect();
+    try{
+        await client.query("BEGIN");
+        await client.query(query, [
             project.id,
             project.title,
             project.description,
@@ -19,48 +20,29 @@ exports.editProject = function (project, callback) {
             project.project_link,
             project.video_link,
             project.applyable
-        ],
-        function (err) {
-            db.query(oldLink, [project.id], function (err) {
-                if (err) {
-                    console.log(err);
-                    callback(err);
-                }
-            });
-            if (err) {
-                console.log(err);
-                callback(err);
-            } else if (project.team != null && project.team.length > 0) {
-                // make sure callback is only called after loop finish
-                Promise.all(project.team.map(function (member) {
-                    return new Promise(function (resolve, reject) {
-                        db.query(newLink, [project.id, parseInt(member.id), member.user_role], function (err) {
-                            if (err) {
-                                return reject(err);
-                            }
-                            resolve();
-                        });
-                    });
-                })).then(function () {
-                    callback();
-                }, function (err) {
-                    console.log(err);
-                    callback(err);
-                });
-            } else {
-                callback();
-            }
+        ]);
+        await client.query(oldLink, [project.id]);
+        for (var i = 0; i < project.team.length; i++){
+            await client.query(newLink, [project.id, parseInt(project.team[i].id), project.team[i].user_role]);
         }
-    );
+        await client.query("COMMIT");
+    }catch(err){
+        await client.query('ROLLBACK');
+        throw err;
+    }finally{
+      client.release(); 
+    }
 };
 
-exports.createProject = function (project, callback) {
-    var query = `INSERT INTO project (title, description, contact, org_name, creation_time, status, project_link, video_link, applyable) 
+exports.createProject = async (project) => {
+    let query = `INSERT INTO project (title, description, contact, org_name, creation_time, status, project_link, video_link, applyable) 
                 VALUES ($1,$2,$3,$4,(now() at time zone 'America/Chicago'),$5, $6, $7, $8) returning id;`;
-    var link = `INSERT INTO user_role(associated_project_id, user_id, user_role) VALUES ($1,$2,$3);`;
-    db.query(
-        query,
-        [
+    let link = `INSERT INTO user_role(associated_project_id, user_id, user_role) VALUES ($1,$2,$3);`;
+    
+    const client = await db.connect();
+    try{
+        await client.query('BEGIN');
+        var projectId = await client.query(query, [
             project.title,
             project.description,
             project.contact,
@@ -69,33 +51,20 @@ exports.createProject = function (project, callback) {
             project.project_link,
             project.video_link,
             project.applyable
-        ],
-        function (err, projectId) {
-            if (err) {
-                console.log(err);
-                callback(err);
-            } else if (project.team != null && project.team.length > 0) {
-                // make sure callback is only called after loop finish
-                Promise.all(project.team.map(function (member) {
-                    return new Promise(function (resolve, reject) {
-                        db.query(link, [projectId.rows[0].id, parseInt(member.id), member.user_role], function (err) {
-                            if (err) {
-                                return reject(err);
-                            }
-                            resolve();
-                        });
-                    });
-                })).then(function () {
-                    callback(null, projectId.rows[0].id);
-                }, function (err) {
-                    console.log(err);
-                    callback(err);
-                });
-            } else {
-                callback(null, projectId.rows[0].id);
-            }
+        ]);
+       
+        for (var i = 0; i < project.team.length; i++){
+            await client.query(link, [projectId.rows[0].id, parseInt(project.team[i].id), project.team[i].user_role]);
         }
-    );
+        await client.query('COMMIT');
+        return projectId.rows[0].id;
+    } catch(err){
+        await client.query('ROLLBACK');
+        throw err;
+    } finally{
+        client.release();
+    }
+   
 };
 
 exports.getProjectSet = function (callback) {
@@ -195,29 +164,23 @@ exports.checkApplied = function (project_id, user_id, callback) {
 }
 
 // the project manager approves the application request
-exports.approveApplicant = function (project_id, user_id, user_role, callback) {
+exports.approveApplicant = async (project_id, user_id, user_role) => {
     // when manager approves the application
     // add relation to project relation
     const query = `INSERT INTO user_role (associated_project_id, user_id, user_role) VALUES($1,$2,$3);`;
     const remove = `DELETE FROM project_application_relation WHERE project_id = $1 AND user_id = $2`;
-    db.query(query, [project_id, user_id, user_role], function (err) {
-        if (err) {
-            console.log(err);
-            callback(err);
-        }
-        else {
-            // if insertion has no error, delete the relationship in application table
-            db.query(remove, [project_id, user_id], function (err) {
-                if (err) {
-                    console.log(err);
-                    callback(err);
-                }
-                else {
-                    callback(null);
-                }
-            });
-        }
-    });
+    const client = await db.connect();
+    try{
+        await client.query('BEGIN');
+        await client.query(query, [project_id, user_id, user_role]);
+        await client.query(remove, [project_id, user_id]);
+        await client.query('COMMIT');
+    } catch (err){
+        await client.query('ROLLBACK');
+        throw err;
+    } finally {
+        client.release();
+    }
 };
 
 // the project manager rejects the application request
