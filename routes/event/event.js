9ad0f7ba0426487e342e725ae9d6109ee33902ee
Reid Chen <mrr1vfe@gmail.com>
2021-01-22 01:05:48 -0600

    Rely on user_event rather than user_profile

--- routes/event/event.js
+++ routes/event/event.js
@@ -7,54 +7,53 @@ const authService = require('../services/authorization_service');
 
 router.get('/', function (req, res, next) {
     let uid = req.session.uid;
-    event_db.getEventSet(function (err, eventsSet) {
+    event_db.getEventSet(function (err, events_set) {
         if (err) {
             console.log(err);
             res.status(400).json({msg: 'Database Error'});
             return;
         }
-        user_db.hasProfile(uid, (err, predicate) => {
+        user_db.getUserRoleByUid(uid, (err, user_role) => {
             if (err) {
+                console.log(err);
                 res.status(400).json({msg: 'Database Error'});
                 return;
             }
-            if (predicate === 'TRUE' || predicate === 'NO_USR_ID') {
-                user_db.getUserFollowedEventsByUid(uid, (err, followed_event) => {
+            user_db.getUserFollowedEventsByUid(uid, (err, followed_events) => {
+                if (err) {
+                    console.log(err);
+                    res.status(400).json({msg: 'Database Error'});
+                    return;
+                }
+                user_db.getUserRSVPEventsByUid(uid, (err, rsvp_events) => {
                     if (err) {
+                        console.log(err);
                         res.status(400).json({msg: 'Database Error'});
                         return;
                     }
-                    user_db.getUserRoleByUid(req.session.uid, function (err, user_role) {
-                        if (err) {
-                            res.status(400).json({msg: 'Database Error'});
-                            return;
+                    events_set = events_set.sort((event_a, event_b) => {
+                        return event_a.event_time - event_b.event_time
+                    })
+                    let now = new Date();
+                    now = new Date(now - (5.5 * 60 * 60 * 1000));
+                    let center_idx = events_set.length - 1;
+                    for (let i = events_set.length - 1; i >= 0; i--) {
+                        if (events_set[i].event_time < now) {
+                            break;
                         }
-                        eventsSet = eventsSet.sort(function (a, b) {
-                            return a.event_time - b.event_time
-                        })
-                        let now = new Date();
-                        now = new Date(now - (5.5 * 60 * 60 * 1000));
-                        var centerIdx = eventsSet.length - 1;
-                        for (var i = eventsSet.length - 1; i >= 0; i--) {
-                            // show the event thats coming up soon
-                            if (eventsSet[i].event_time < now) {
-                                break;
-                            }
-                            centerIdx = i;
-                        }
-                        res.render('event/index', {
-                            followed_event: JSON.parse(followed_event),
-                            eventsSet: eventsSet,
-                            uid: req.session.uid,
-                            all_user_role: authService.UserRole,
-                            user_role: user_role,
-                            centerIdx: centerIdx
-                        });
+                        center_idx = i;
+                    }
+                    res.render('event/index', {
+                        followed_events: JSON.parse(followed_events),
+                        rsvp_events: JSON.parse(rsvp_events),
+                        eventsSet: events_set,
+                        uid: req.session.uid,
+                        all_user_role: authService.UserRole,
+                        user_role: user_role,
+                        centerIdx: center_idx
                     });
                 });
-            } else {
-                res.redirect('../profile?status=f&msg=Please_update_your_profile');
-            }
+            });
         });
     });
 });
